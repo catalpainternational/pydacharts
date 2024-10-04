4c059e3201000d3bc23fa44864fc76da4b460bde
Josh Brooks <josh.vdbroek@gmail.com>
2024-10-04 01:38:19 -0400

    better test layout

--- pydacharts/test_chart_utils.py
+++
@@ -1,6 +0,0 @@
-from pydacharts.chart_utils import HorizontalBarChartStackedOptions, DoughnutChartOptions, BarChartOptions, PyramidChartOptions
-import pytest
-
-@pytest.mark.parametrize("element", [HorizontalBarChartStackedOptions, DoughnutChartOptions, BarChartOptions, PyramidChartOptions])
-def test_chart_utils(element):
-    element().model_dump_json(exclude_none=True, indent=1)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,26 +1,50 @@
-from pydacharts.models import Layout, Interaction, Scales, ScaleOptions, Ticks, Tooltip, TooltipCallbacks, Legend, \
-    LegendTitle, LegendLabels, Title, Dataset, Data, Font, Plugins, Grid, Options, Config
+from pydacharts.models import (
+    Layout,
+    Interaction,
+    Scales,
+    ScaleOptions,
+    Ticks,
+    Tooltip,
+    TooltipCallbacks,
+    Legend,
+    LegendTitle,
+    LegendLabels,
+    Title,
+    Dataset,
+    Data,
+    Font,
+    Plugins,
+    Grid,
+    Options,
+    Config,
+)
 from pydacharts.plugins.datalabels import DataLabelsPlugin
 
 import pytest
-@pytest.mark.parametrize("testcase", [
-    Layout,
-    Font,
-    Title,
-    LegendLabels,
-    LegendTitle,
-    Legend,
-    DataLabelsPlugin,
-    TooltipCallbacks,
-    Tooltip,
-    Plugins,
-    Ticks,
-    Grid,
-    ScaleOptions,
-    Scales,
-    Interaction,
-    Options,
-    Config,])
+
+
+@pytest.mark.parametrize(
+    "testcase",
+    [
+        Layout,
+        Font,
+        Title,
+        LegendLabels,
+        LegendTitle,
+        Legend,
+        DataLabelsPlugin,
+        TooltipCallbacks,
+        Tooltip,
+        Plugins,
+        Ticks,
+        Grid,
+        ScaleOptions,
+        Scales,
+        Interaction,
+        Options,
+        Config,
+    ],
+)
 def test_all_models(testcase):
     testcase().model_dump_json()
 
