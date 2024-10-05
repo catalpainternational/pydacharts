4aea2b72877f510da1b9f4474f28074a6c10d35d
Josh Brooks <josh.vdbroek@gmail.com>
2024-10-05 22:40:26 +0900

    Replace flaksk with fastapi for native Pydantic requests

--- serve/app.py
+++ serve/app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydacharts.models import Config
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
 
 from .serve_data import config
 
-app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
     config_json = config().model_dump_json(exclude_none=True, indent=2)
-    return render_template(
-        "chart_datalabels.html",
-        config=config_json,
+    return templates.TemplateResponse(
+        request=request, name="chart_datalabels.html", context={"config": config_json}
     )
+
+
+@app.get("/chart", response_model=Config, response_model_exclude_none=True)
+async def read_item(request: Request):
+    return config()
